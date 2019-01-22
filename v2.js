50576827c46e4adec8186b32ce6365118f11e6f4
Andrew Stanton <up683693@myport.ac.uk>
2019-01-22 10:44:36 +0000

    First attempt of composing the summary

--- v2.js
+++ v2.js
@@ -6,21 +6,28 @@ const stringify = require('csv-stringify')
 let streamIn = fs.createReadStream(__dirname + '/UG19Entry.csv');
 let streamOut = fs.createWriteStream(__dirname + '/UGOut.csv');
 
+let makeList = (reqs, sep = '\n', tag = 'ul', itemTags = 'li') => `<${tag}>\n${[...reqs.split(sep).map((r) => `<${itemTags}>${r}</${itemTags}>`)].join('\n')}\n</${tag}>`
+
 const stringifier = stringify();
 stringifier.on('readable', () => {
-  let row;
-  while(row = stringifier.read()){
-    streamOut.write(row);
-  }
+	let row;
+	while(row = stringifier.read()){
+			streamOut.write(row);
+	}
 })
 stringifier.on('error', (err) => {
-  console.error(err.message)
+	console.error(err.message)
 })
 
 streamIn.pipe(parse({columns: true}))
-    .on('data', (csvrow) => {
-        stringifier.write(csvrow);
-    })
-    .on('end', () => {
-      	streamOut.end();
-    });
+	.on('data', (csvrow) => {
+		let {entryReq, process} = csvrow;
+		let quals = `<p>Qualifications</p>\n${makeList(entryReq)}`;
+		let selection = `<p>Selection Process</p>\n${makeList(process)}`;
+		csvrow.summary = `${quals}\n${selection}`;
+		console.log(csvrow);
+		stringifier.write(csvrow);
+	})
+	.on('end', () => {
+		streamOut.end();
+	});
