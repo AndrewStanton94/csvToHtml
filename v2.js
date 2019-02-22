d64ae1d81669f16a4526256e08dfed9f0cdb1432
Andrew Stanton <up683693@myport.ac.uk>
2019-02-22 13:08:24 +0000

    Updated text, headers and spacing

--- v2.js
+++ v2.js
@@ -1,20 +1,22 @@
 const fs = require("fs");
 const parse = require("csv-parse");
-// const transform = require('stream-transform');
 const stringify = require("csv-stringify");
 
+// Stream delarations
 let streamIn = fs.createReadStream(__dirname + "/UG19Entry.csv");
 let streamOut = fs.createWriteStream(__dirname + "/UGOut.csv");
 
+// Utility functions
 let makeList = (reqs, sep = "\n", tag = "ul", itemTags = "li") =>
   `<${tag}>\n${[
-    ...reqs.split(sep).map(r => `<${itemTags}>${r}</${itemTags}>`)
+    ...reqs.split(sep).map(r => `\t<${itemTags}>${r}</${itemTags}>`)
   ].join("\n")}\n</${tag}>`;
 
 const makeCourseLink = (code, text, year=2019) => `<a href="https://www.registryhub.port.ac.uk/entry_requirements/?course_code=${code}&year=${year}" target="_blank" title="Opens in a new tab">${text}</a>`
 const requirementsLink = (code) => `\n<p>${makeCourseLink(code, 'See the other qualifications we accept')}</p>`;
 const languageLink = (code) => `\n<p>${makeCourseLink(code, 'See alternative English language qualifications')}</p>`;
 
+// Setting up the write out code
 const stringifier = stringify();
 stringifier.on("readable", () => {
   let row;
@@ -26,17 +28,18 @@ stringifier.on("error", err => {
   console.error(err.message);
 });
 
+// Take and processes data from input stream
 streamIn
   .pipe(parse({ columns: true }))
   .on("data", csvrow => {
     let { code, entryReq, process, language } = csvrow;
     code = code.trim();
-    let quals = `<h5>Qualifications</h5>\n${makeList(entryReq)}${requirementsLink(code)}`;
-    let lang = `<h5>Language</h5>\n${makeList(language)}${languageLink(code)}`;
+    let quals = `<h4>Qualifications or experience</h4>\n${makeList(entryReq)}${requirementsLink(code)}`;
+    let lang = `<h4>English language requirements</h4>\n${makeList(language)}${languageLink(code)}`;
     let selection = process
-      ? `<h5>Selection Process</h5>\n${makeList(process)}`
+      ? `<h4>Selection Process</h4>\n${makeList(process)}`
       : "";
-    csvrow.summary = `${quals}\n${lang}\n${selection}`;
+    csvrow.summary = `${quals}\n\n${lang}\n\n${selection}`;
     console.log(csvrow);
     stringifier.write(csvrow);
   })
